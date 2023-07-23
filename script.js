d88c3f80c83605b77c807a1606e098ae497c6d18
Pracheeyadav2022 <108492578+Pracheeyadav2022@users.noreply.github.com>
2023-07-23 16:00:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -80,4 +80,30 @@ function showQuestion(){
         button.addEventListener("click",selectAnswer);
     });
 }
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct"); 
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button =>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+
+        }
+        button.disabled = true;
+
+    
+});
+nextButton.style.display = "block";
+}
+startQuiz();
 
